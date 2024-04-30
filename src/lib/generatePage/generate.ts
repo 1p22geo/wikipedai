2d31cb03c97f1324615d375e92accb9be41ee1c2
1p22geo <1p22geodecki@gmail.com>
2024-04-30 17:02:17 +0200

    fix: more optimisations

--- src/lib/generatePage/generate.ts
+++ src/lib/generatePage/generate.ts
@@ -1,20 +1,13 @@
-import { MongoClient } from "mongodb"
-import { promptCache } from "@wiki/model/promptCache"
 import prompts from "@wiki/prompts/prompts.json"
 import { ollamaPrompt } from "../prompt"
+import { savePage } from "../savePage"
 
 export async function generate(title: string) {
-  const client = new MongoClient(process.env.MONGO_URI ?? "")
-  await client.connect()
-  const db = client.db("wikipedai")
-  const cache = db.collection<promptCache>("promptCache")
 
   let content: string = await ollamaPrompt(prompts.generatePage.replace("%%%", title))
   if (content.endsWith("```")) {
     content = content.split("\n").slice(1, -1).join("\n")
   }
-  const res: promptCache = { title, ready: true, content }
-  await cache.findOneAndReplace({ title }, { ...res })
-  await client.close()
-  return res
+
+  return await savePage(title, content)
 }
---
+++ src/lib/savePage/index.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb"
+import { promptCache } from "@wiki/model/promptCache"
+export async function savePage(title: string, content: string) {
+  const client = new MongoClient(process.env.MONGO_URI ?? "")
+  await client.connect()
+  const db = client.db("wikipedai")
+  const cache = db.collection<promptCache>("promptCache")
+
+  const res: promptCache = { title, ready: true, content }
+  await cache.findOneAndReplace({ title }, { ...res })
+  await client.close()
+  return res
+
+}
